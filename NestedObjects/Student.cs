09fe55141d1abe9ba9b6c70ea0e81c5fa38d6dae
wague <wague@WARRENGUERTIN>
2023-07-18 20:09:59 -0700

    Created student and advisor classes

---
+++ NestedObjects/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents an individual college student
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// The legal first name 
+        /// </summary>
+        public string FirstName { get; set; }
+
+
+        /// <summary>
+        /// The legal last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The assigned school email address
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        /// <summary>
+        /// Contact phone number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+
+    }
+}
